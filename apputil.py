8b6c5d33536d4efba584fb168f0f63788cb6093c
Noor <nhassuneh1@gmail.com>
2025-11-01 23:28:57 -0400

    np error still lol

--- apputil.py
+++ apputil.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 import pickle
+import numpy as np
 
 # Load data
 url = "https://raw.githubusercontent.com/leontoddjohnson/datasets/refs/heads/main/data/coffee_analysis.csv"
