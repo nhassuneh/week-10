6d21d0a76654d1be3e36f5466fc38f7d245618de
Noor <nhassuneh1@gmail.com>
2025-11-01 23:26:43 -0400

    np error when I didnt have np so I added it

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
+import numpy as np
 
 def train_models():
     """
